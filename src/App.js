7379cf95751e01c1c2f9fceb0d885b7ce73532fa
Robin Claes <robin.claes@icapps.com>
2019-09-04 11:17:28 +0200

    add font and elements

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import logo from "./logo.svg";
+import {
+  CardNumberElement,
+  CardExpiryElement,
+  CardCVCElement,
+  StripeProvider,
+  Elements
+} from "react-stripe-elements";
+import "./App.css";
+
+const options = {
+  base: {
+    fontFamily: "customFont",
+    fontSize: "18px",
+    fontSmoothing: "antialiased",
+    letterSpacing: "0.025em"
+  }
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <StripeProvider apiKey="123456789">
+        <Elements
+          fonts={[
+            {
+              src: `${process.env.PUBLIC_URL}/font.ttf`,
+              family: "customFont"
+            }
+          ]}
+        >
+          <Fragment>
+            <CardNumberElement style={options} onChange={() => null} />
+            <CardExpiryElement style={options} onChange={() => null} />
+            <CardCVCElement style={options} onChange={() => null} />
+          </Fragment>
+        </Elements>
+      </StripeProvider>
     </div>
   );
 }
